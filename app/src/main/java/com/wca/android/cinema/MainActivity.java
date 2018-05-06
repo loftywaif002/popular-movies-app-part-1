9261847255b048e53a65631a9f3994cb505cdfcf
loftywaif002 <deep.row4@gmail.com>
2018-05-05 22:42:28 -0700

    fetch data is done

--- app/src/main/java/com/wca/android/cinema/MainActivity.java
+++ app/src/main/java/com/wca/android/cinema/MainActivity.java
@@ -1,35 +1,47 @@
 package com.wca.android.cinema;
 
-import android.graphics.Movie;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import utilities.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static final String TAG = NetworkUtils.class.getSimpleName();
 
     @BindView(R.id.indeterminateBar)
     ProgressBar mProgressBar;
 
-    String popularMovies;
-    String topRatedMovies;
+    String popularMoviesURL;
+    String topRatedMoviesURL;
 
     ArrayList<Movie> mPopularList;
     ArrayList<Movie> mTopTopRatedList;
+
+    public Boolean shouldCall = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        mProgressBar.setVisibility(View.INVISIBLE); //Hide Progressbar by Default
+
+       //if(shouldCall) { // I know if the method has been called before
+        // new FetchMovies().execute();
+         // shouldCall = false;
+       //}
+
     }
 
     //AsyncTask
@@ -44,13 +56,28 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... voids) {
 
-            popularMovies = "https://api.themoviedb.org/3/movie/popular?api_key=525e39d1c3568cd23cdaf0a3674918fa&language=en-US&page=1";
-            topRatedMovies = "https://api.themoviedb.org/3/movie/top_rated?api_key=525e39d1c3568cd23cdaf0a3674918fa&language=en-US&page=1";
+            popularMoviesURL = "https://api.themoviedb.org/3/movie/popular?api_key=525e39d1c3568cd23cdaf0a3674918fa&language=en-US&page=1";
+            topRatedMoviesURL = "https://api.themoviedb.org/3/movie/top_rated?api_key=525e39d1c3568cd23cdaf0a3674918fa&language=en-US&page=1";
 
             mPopularList = new ArrayList<>();
             mTopTopRatedList = new ArrayList<>();
 
+            try {
+                mPopularList = NetworkUtils.fetchData(popularMoviesURL);
+                mTopTopRatedList = NetworkUtils.fetchData(topRatedMoviesURL);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void  s) {
+            super.onPostExecute(s);
+            mProgressBar.setVisibility(View.INVISIBLE);
+
         }
     }
 
